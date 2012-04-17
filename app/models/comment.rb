a26744858f84a5999067679b22b1a236e383f54d
Brad Charna <bcharna@andrew.cmu.edu>
2012-04-16 20:07:34 -0400

    added views/controller

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
+  
   validates_presence_of :content
   validates_presence_of :name
   validates_presence_of :email
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120416235555) do
+ActiveRecord::Schema.define(:version => 20120417000520) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
