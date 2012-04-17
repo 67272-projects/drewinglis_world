2c48ed4612c76183b9ac77fc022ad700c0939860
Brad Charna <bcharna@andrew.cmu.edu>
2012-04-16 20:01:48 -0400

    added validation

---
+++ app/models/comment.rb
@@ -0,0 +1,9 @@
+class Comment < ActiveRecord::Base
+  validates_presence_of :content
+  validates_presence_of :name
+  validates_presence_of :email
+  
+  validates_format_of :email, :with => /^[\w]([^@\s,;]+)@(([a-z0-9.-]+\.)+(com|edu|org|net|gov|mil|biz|info))$/i, :message => "is not a valid format", :allow_blank => true
+  
+  
+end
